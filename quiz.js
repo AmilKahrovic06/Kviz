4248738ddc81c1650abe8907bce04e554d0c848f
trtovacasmir <trtovacasmir@gmail.com>
2023-04-25 18:16:15 +0200

    update

--- quiz.js
+++ quiz.js
@@ -9,6 +9,14 @@ promesaj();
 
   let pitanje=document.querySelector(".question");
   
-  questions.answers.forEach(element => {
-    document.write(questions[element])
+//   questions.answers.forEach(element => {
+//     document.write(questions[element])
+//   });
+
+questions.forEach((question) => {
+    question.answers.forEach((answer) => {
+      const button = document.createElement('button');
+      button.textContent = answer;
+      pitanje.appendChild(button);
+    });
   });
