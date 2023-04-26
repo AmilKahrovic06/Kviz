aca01c39e6b57aaf6165a850489c9000b269aa15
trtovacasmir <trtovacasmir@gmail.com>
2023-04-26 18:52:57 +0200

    update

--- quiz.js
+++ quiz.js
@@ -1,5 +1,6 @@
 console.log(questions);
-
+let odgovorio = false
+let tacan = false;
 function promesaj() {
   questions.sort(function (a, b) {
     return Math.random() - 0.5;
@@ -7,11 +8,14 @@ function promesaj() {
 }
 promesaj();
 
+
+
+
 let odgovori = document.querySelector(".answers");
 let pitanje = document.querySelector(".question");
 console.log(pitanje);
 
-const questionIndex = 0;
+let questionIndex = 0;
 const tekstPitanja = document.createElement("h3");
 tekstPitanja.className = "question_js";
 tekstPitanja.innerHTML = questions[questionIndex].question;
@@ -20,10 +24,14 @@ function answers() {
   pitanje.appendChild(tekstPitanja);
   
   questions[questionIndex].answers.forEach((answer) => {
+    console.log(answer);
+    
   const button = document.createElement("button");
   button.className = "ans_button";
   button.textContent = answer;
+
   button.addEventListener("click", () => {
+        console.log(button.innerText);
     const modal = document.createElement("div");
     modal.className = "modal";
     const modalContent = document.createElement("div");
@@ -35,13 +43,27 @@ function answers() {
     modalButtons.className = "modal-buttons";
     const yesButton = document.createElement("button");
     yesButton.textContent = "Da";
+
     yesButton.addEventListener("click", () => {
   
-      console.log("Odgovor potvrđen!");
-      questionIndex++;
+    console.log("Odgovor potvrđen!");
+    let index = questions[questionIndex].answers.indexOf(button.innerText);
+    console.log(index)
+      if (index=== questions[questionIndex].correct_answer) {    
+        console.log('tacan')
+        button.style.backgroundColor = 'green'
+        tacan = true;     
+    }
+      else{
+        button.style.backgroundColor = 'red'
+        console.log('nije tacan')
+        tacan = false;
+      }
 
 
       modal.style.display = "none";
+      odgovorio=true;
+      disableButton();
     });
     const noButton = document.createElement("button");
     noButton.textContent = "Ne";
@@ -70,8 +92,22 @@ function answers() {
 
 function prikaziPitanje() {
 odgovori.innerHTML = "";
+console.log(questionIndex)
 tekstPitanja.innerHTML = questions[questionIndex].question;
+
 answers();
 }
 
 prikaziPitanje();
+
+
+function disableButton(){
+    const btn = document.querySelectorAll(".ans_button");
+    if(odgovorio){
+
+        btn.forEach(el=>{
+            el.disabled = true;
+        })
+    }
+
+}
